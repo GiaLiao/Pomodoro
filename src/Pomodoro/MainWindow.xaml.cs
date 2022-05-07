7cfd2c8ed5c9be127318e792556131110a34345f
Gia <81024664+GiaLiao@users.noreply.github.com>
2022-05-07 19:28:25 +0800

    UI initialization

--- src/Pomodoro/MainWindow.xaml.cs
+++ src/Pomodoro/MainWindow.xaml.cs
@@ -15,14 +15,69 @@ using System.Windows.Shapes;
 
 namespace Pomodoro
 {
+    public enum Status
+    {
+        Pomodoro,
+        ShortBreak,
+        LongBreak
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        Status status = Status.Pomodoro;
+        int intervalCounter = 0;
+        int finishedCounter = 0;
+        int pomodorosPerRound = 4;
+        List<int> intervals = new List<int> { 25, 5, 30 };
+        TimeSpan intervalTime;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            // update UI
+            UpdateWithStatus();
+            UpdateWithFinishedCounter();
+        }
+
+        private void UpdateWithStatus()
+        {
+            int interval = intervals[(int)status];
+            intervalTime = TimeSpan.FromMinutes(interval);
+            timeDisplayText.Text = GetDisplayTime(intervalTime);
+            statusText.Text = GetStatusText();
+        }
+
+        private void UpdateWithFinishedCounter()
+        {
+            finishText.Text = $"Finished: {finishedCounter}";
+        }
+
+        private string GetStatusText()
+        {
+            switch (status)
+            {
+                case Status.Pomodoro:
+                    return "Pomodoro";
+                case Status.ShortBreak:
+                    return "Short Break";
+                case Status.LongBreak:
+                    return "Long Break";
+                default:
+                    break;
+            }
+            return String.Empty;
+        }
+
+        private string GetDisplayTime(TimeSpan time)
+        {
+            return String.Format($@"{(int)time.TotalMinutes:00}:{time.Seconds:00}");
         }
     }
 }
